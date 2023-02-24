e5c1d6f20848b8f2d8c82a628d033421dfc84c78
Konika Negi <Konika.Negi@einfochips.com>
2023-02-24 11:14:46 +0530

    click,type,select

--- demo1_basics/demo2_register.py
+++ demo1_basics/demo2_register.py
@@ -2,8 +2,10 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import select
+from selenium.webdriver.support.select import Select
 
-driver=webdriver.Chrome()
+driver=webdriver.Firefox()
 driver.get("https://facebook.com/")
 driver.maximize_window()
 #i fpage loading takes time, it waits for the controls to be visible
@@ -13,23 +15,26 @@ driver.implicitly_wait(30)
 driver.find_element(By.LINK_TEXT,"Create new account").click()
 driver.find_element(By.NAME,"firstname").send_keys("John")
 driver.find_element(By.NAME,"lastname").send_keys("Wick")
+driver.find_element(By.NAME,"reg_email__").send_keys("kokoa@gmail.com")
+driver.find_element(By.NAME,"reg_email_confirmation__").send_keys("kokoa@gmail.com")
 driver.find_element(By.ID,"password_step_input").send_keys("welcome@@123")
-driver.find_element(By.XPATH,"//input[@value='-1']").click()
-time.sleep(5)
+driver.find_element(By.XPATH,"//input[@value='2']").click()
 driver.find_element(By.XPATH,"//select[@id='day']").click()
-#driver.find_element(By.XPATH,"//select[text()='2']").click()
+#driver.find_element(By.ID,"day").send_keys("30")
+#driver.find_element(By.ID,"month").send_keys("Apr")
+#driver.find_element(By.ID,"year").send_keys("2000")
+driver.find_element(By.NAME,"websubmit").click()
+
+#dropdown
+select_day=Select(driver.find_element(By.ID,"day"))
+select_day.select_by_value("12")
+
+select_month=Select(driver.find_element(By.ID,"month")) # also can use XPATH //select[@title="Month"]
+select_month.select_by_visible_text("12")
 time.sleep(5)
 
+select_year=Select(driver.find_element(By.ID,"year"))
+select_year.select_by_visible_text("2019")
+
+#Select(driver.find_element(By.ID,"year")).select_by_value("2019")
 #time.sleep(5)
-#driver.find_element(By.XPATH,"//select[@text()='2']").click()
-#time.sleep(5)
-
-
-
-
-#20 dec 2000 dropdow
-
-#driver.find_element(By.ID,"//select[@day='3']").click()
-
-
-driver.implicitly_wait(10)
---
+++ demo3_salesforce.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver=webdriver.Chrome()
+driver.get("https://www.salesforce.com/in/form/signup/freetrial-sales/")
+driver.maximize_window()
+driver.implicitly_wait(30)
+
+driver.find_element(By.NAME,"UserFirstName").send_keys("Konika")
+driver.find_element(By.NAME,"UserLastName").send_keys("Negi")
+Select(driver.find_element(By.NAME,"UserTitle")).select_by_value("IT_Manager_AP")
+Select(driver.find_element(By.NAME,"CompanyEmployees")).select_by_visible_text("101 - 500 employees")
+Select(driver.find_element(By.NAME,"CompanyCountry")).select_by_value("GB")
+driver.find_element(By.XPATH,"//div[@class='checkbox-ui']").click()
+driver.find_element(By.NAME,"start my free trial").click()
